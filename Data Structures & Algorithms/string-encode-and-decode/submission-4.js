c4f38631eb682d2d391feb5d3a04e1d94f6eebd8
Jaime Andres Alvarez <113536474+JaimeAlvarez90@users.noreply.github.com>
2026-05-19 13:13:16 -0500

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const encoded = strs.map((str)=>{
+            return `${str.length}#${str}`;
+        }).join("");
+        console.log('encoded',encoded)
+        return encoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        //5#Hello5#World
+   const result = [];
+    let i = 0;
+    while (i < str.length) {
+      let j = i;
+      while (str[j] !== "#") j++;
+      const length = parseInt(str.slice(i), 10);
+      console.log("length", length);
+      const start = j + 1;
+      result.push(str.slice(start, start + length));
+      i = start + length;
+    }
+    return result;
+    }
+}
